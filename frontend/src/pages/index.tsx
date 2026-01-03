76859d419dfa9bc344b33f91dabfe0396f3fd1aa
Paguiel <119151331+paguielng@users.noreply.github.com>
2026-01-03 22:32:56 +0100

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,1 +1,13 @@
+import type { NextPage } from 'next';
+import Player from '../components/Player';
 
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur ts-migrate</h1>
+      <Player />
+    </div>
+  );
+};
+
+export default Home;
