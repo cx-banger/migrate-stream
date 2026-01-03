695ab2dfe1c4c5978be9857c6fea24110fe887c6
Paguiel <119151331+paguielng@users.noreply.github.com>
2026-01-03 22:34:45 +0100

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,14 @@
+package main
 
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Backend Go prêt pour ts-migrate!")
+    })
+    fmt.Println("Serveur Go démarré sur : http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
